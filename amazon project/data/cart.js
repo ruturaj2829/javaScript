079ce2c088424b28025a96bbeca1b5b4524d2dc1
ruturaj2829 <ruturajsonkamble-ecs@atharvacoe.ac.in>
2023-10-13 22:59:41 +0530

    sixth commit

--- amazon project/data/cart.js
+++ amazon project/data/cart.js
@@ -1,1 +1,21 @@
-const cart = [];
+export const cart = [{
+
+}];
+export function addToCart (){
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+      if (products === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: products,
+        quantity: 1
+      });
+    }
+}
